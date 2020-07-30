4e40e7238a99af9dabc0f51665a6c316ae9d1d8b
Franco Aguzzi (Valtech) <franco.aguzzi@valtech.com>
2020-07-30 19:05:47 -0300

    styling homepage

--- mobile/src/pages/HomePage.js
+++ mobile/src/pages/HomePage.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { TextInput, Text, Button, StyleSheet } from 'react-native';
 
-import { Header } from '../components/Header.js';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default Homepage = () => {
     return (
-        <SafeAreaView>
-            <Header />
-            <Text>Homepage</Text>
+        <SafeAreaView style={ styles.safeArea }>
+            <Text style={ styles.header }>Bem-vindo,</Text>
+            <Text style={ styles.subtitle }>Informe seus dados para acessar o bicicletário.</Text>
+            <Text style={ styles.label }>Matrícula UFSC:</Text>
+            <TextInput style={ styles.input }/>
+            <Text style={ styles.label }>Senha:</Text>
+            <TextInput style={ styles.input }/>
+            <Button title="Entrar" style={ styles.cta }/>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    safeArea: {
+        backgroundColor: '#ffcb11',
+        flexGrow: 1
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    subtitle: {
+        fontSize: 16
+    },
+    label: {
+        fontSize: 18
+    },
+    input: {
+        backgroundColor: '#fff',
+        padding: '10px 50px'
+    },
+    cta: {
+        backgroundColor: '#2364aa',
+        padding: '20px 35px'
+    }
+})
